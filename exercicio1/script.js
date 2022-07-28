f6a3310e123ae958657c5ab2c8a1a11f53bdb0f0
Alesouza7 <alexcostasouza86@yahoo.com>
2022-07-28 19:00:45 -0300

    atualização

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,1 +1,6 @@
 const numeros= [29,59,28,73,20]
+const strings=["pedra","papel","tesoura"]
+const array3=[33,"telefone",true]
+console.log(numeros[1]);
+console.log(strings[2]);
+console.log(array3[1]);
